6168903c090230535038c145238e143621378ce0
bruno boutin <b.boutin@uva.nl>
2021-04-29 16:29:26 +0200

    Create mergePR.sh

---
+++ .github/mergePR.sh
@@ -0,0 +1,8 @@
+put_url="https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_ID/merge"
+echo "${put_url}"
+curl_reply=$(curl \
+		        -X PUT \
+		        $put_url \
+		        -d '{"commit_title":"myCommit", "merge_method": "squash"}' \
+        		-H "Authorization: Bearer $TOKEN")
+echo "$curl_reply"
