2e9c9f27142fa8b94447b515109113139e5d0589
bruno boutin <b.boutin@uva.nl>
2021-04-30 16:05:04 +0200

    Update mergePR.sh

--- .github/mergePR.sh
+++ .github/mergePR.sh
@@ -1,8 +1,28 @@
-put_url="https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_ID/merge"
-echo "${put_url}"
-curl_reply=$(curl \
-		-X PUT \
-		$put_url \
-		-d '{"commit_title":"myCommit", "merge_method": "squash"}' \
-       		-H "Authorization: Bearer $TOKEN")
-echo "$curl_reply"
+owner_repo="$1"
+author='weblate'
+
+function getPRNumber()
+{ 
+  pr_list=$(gh pr list -R $owner_repo -A $author)
+  first_line=$(echo "$pr_list" | sed -n '1p')
+  number=$(echo "$first_line" | grep -oE '^[0-9]+')
+  echo "$number"
+}
+
+pr_number="$(getPRNumber)"
+if [ -z "$pr_number" ]
+then
+  echo "No Pull Request"
+else
+  echo "Pull Request ID: $pr_number"
+  echo $(gh pr merge --squash $pr_number -R $owner_repo)
+  
+  pr_number="$(getPRNumber)"
+  if [ -z "$pr_number" ]
+  then
+    echo "No other Pull Request"
+  else
+    echo "2nd Pull Request ID: $pr_number"
+    echo $(gh pr merge --squash $pr_number -R $owner_repo)
+  fi
+fi
