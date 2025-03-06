1a000066d058299150ee0439721b288e2c3454a9
Joris Goosen <joris@jorisgoosen.nl>
2025-03-06 16:15:45 +0100

    add format test for percentage

--- R/testTableFormatting.R
+++ R/testTableFormatting.R
@@ -13,7 +13,9 @@ testTableFormattingFunc <- function(jaspResults, dataset, options)
 			someMoney=c(10, 11.00, 100000002, 100.12),
 			someMoneyDollar=c(1043, 11.00, 100000002, 100.12), 
 			someFixedDecimals=c(1.234, 12.34, 123.4, 1234.0 ),
-			someSignificance=c(1.234, 12.34, 1221312313.4, 1234.0 ), 
+			someSignificance=c(1.234, 12.34, 1221312313.4, 1234.0 ),
+			someProcent2=c(0.012345, 0.12345, 1.23456, 12.3456 ), 
+			someProcent=c(0.012345, 0.12345, 1.23456, 12.3456 ), 
 			someString=c("a", "b", "c", "een of andere tekst")))
 
 	# the displayed columns are supposed to be just the number of variable
@@ -22,6 +24,8 @@ testTableFormattingFunc <- function(jaspResults, dataset, options)
 	mainTable$addColumnInfo(name = "someMoney", 		title = "Money", 				type = "number", format="monetary")
 	mainTable$addColumnInfo(name = "someFixedDecimals", title = "Fixed decimals", 		type = "number", format="dp:1")
 	mainTable$addColumnInfo(name = "someSignificance", 	title = "Signify", 				type = "number", format="sf:2")
+	mainTable$addColumnInfo(name = "someProcent2", 		title = "Percentages 2 dp", 	type = "number", format="percentage:2")
+	mainTable$addColumnInfo(name = "someProcent", 		title = "Percentages", 			type = "number", format="percentage")
 	mainTable$addColumnInfo(name = "someString", 		title = "Strings", 				type = "string", format="")
 	mainTable$setData(tableData)
 
